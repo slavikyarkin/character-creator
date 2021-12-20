8886eb306f61eee3ce5d5b606cd69cdb3a1a499f
slavikyarkin <slavikyarkin@gmail.com>
2021-12-20 14:44:05 +0300

    module second: Workshop: Character Creator

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import CharacterEditor from './components/CharacterEditor';
+import Decoration from './components/Decoration';
 import Footer from './components/Footer';
 
 function App() {
   return (
     <>
+      <Decoration />
       <CharacterEditor />
       <Footer />
     </>
---
+++ src/components/Decoration/Decoration.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './Decoration.module.css';
+
+const Decoration = () => {
+  return (
+    <div className={styles.decoration}> 
+
+    </div>
+    );
+};
+
+export default Decoration;
